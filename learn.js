d3f6310f8bad4a3d3945d2913f4039ee93e79743
Jikeh <joshuaikeh16@gmail.com>
2026-02-13 12:31:26 +0100

    my very first commit

---
+++ learn.js
@@ -0,0 +1,12 @@
+// const mass = document.getElementById("message");
+mass.addEventListener("click", function() {
+    alert("You have experienced precision!");
+});
+const ma = document.getElementById("sexy");
+ma.addEventListener("click", function() {
+    alert("Welcome to the world of speed!");
+});
+const mal = document.getElementById("usa");
+mal.addEventListener("click", function() {
+    alert("Hear the roar of the engine!");
+});
